a8f29fbc1913d3b262a54b2028738183a20f11d1
AlexanderTCSE <tattersallalexd@gmail.com>
2024-09-05 21:52:02 +0000

    9.5.24 Lab, program can scroll dictionaries and attempts to pad and search for user's word. Padding not 100% yet.

---
+++ Program2/README.txt
@@ -0,0 +1,8 @@
+//Alexander Tattersall
+//CS360 - Systems Programming
+//Professor Ben McCamish
+//Programming Assignment 2: dictsearch
+
+Program is meant to search a dictionary at a byte level for a specified word
+Will return the line which the word is found on if it exists, or the negative
+of the last line searched for the word.
--- Program2/assignment2.c
+++ Program2/assignment2.c
@@ -5,7 +5,12 @@ description:
 	See CS 360 IO lecture for details.
 ***********************************************************************/
 
-/* Includes and definitions */
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
 
 /**********************************************************************
 Search for given word in dictionary using file descriptor fd.
@@ -13,7 +18,43 @@ Return the line number the word was found on, negative of the last line searched
 if not found, or the error number if an error occurs.
 **********************************************************************/
 int lineNum(char *dictionaryName, char *word, int length) {
-	//insert your code here
+
+	//open file and error check that it exists
+	int fd = open(dictionaryName, O_RDONLY, 0);
+	if(fd<0){
+		printf("File %d not found.\n",fd);
+		exit(1);
+	}
+
+	//initialize buffer and attempt
+	char *buffer = (char *)malloc(length);
+	int count=0;
+	int padding = (sizeof(buffer)-strlen(word));
+	for(int i=0;i<padding;i++){
+		strcat(word," ");
+	}
+	int attempt = sizeof(buffer);
+	int gotWord;
+	//main while loop to read in [attempt] bytes at a time
+	while((gotWord=read(fd,buffer,attempt)) > 0){	
+		count++;		
+		if(strcmp(buffer,word) == 0){
+			printf("Word matched at line <%d>.\n",count);
+			fflush(stdout);
+			exit(0);
+		}	
+		write(1,buffer,gotWord);	
+	}
+	printf("Word not found, reached line <-%d>.\n",count);
+	free(buffer);
+	close(fd);
 	return 0;
 }
 
+int main(int argc, char *argv[]){
+	//mostly a test case to use cmd line arguments
+	char *dict=argv[1];
+	char *word=argv[2];
+	int length=atoi(argv[3]);
+	lineNum(dict,word,length);
+}
