e3580300611fbd38bccfcbebbef5a93624cec4a0
AlexanderTCSE <tattersallalexd@gmail.com>
2024-09-17 19:43:31 +0000

    Program recurses, ignores parent directories. Memory issues need fixing.

--- Program3/assignment3.c
+++ Program3/assignment3.c
@@ -25,7 +25,7 @@ to respond to.
 int readable(char* inputPath) {
 	struct stat area, *s = &area;
 	struct dirent *entry;
-	char *dirP;	
+	DIR *dirP;
 	int fileCount=0;	//counter for readable files, return this
 
 	//handle no input path error case
@@ -34,31 +34,57 @@ int readable(char* inputPath) {
 		printf("No path specified, working from current dir: <%s>\n",inputPath);
 	}
 
-	//if block to check what the current file is
-	if(stat(inputPath,s)==0){
-		if(S_ISREG (s->st_mode)){
-			printf("%s is a regular file.\n",inputPath);
-			if(access(inputPath,R_OK)==0){
-				printf("Can read %s.\n",inputPath);
-				fileCount++;
-			}else{
-				printf("Cannot read %s.\n",inputPath);
-			}
-		}else if(S_ISDIR(s->st_mode)){
-			printf("%s is a directory.\n",inputPath);
-			if(access(inputPath,F_OK)==0){
-				printf("Can access dir: %s.\n",inputPath);
-					
-			}else{
-				printf("Unable to access dir: %s.\n",inputPath);
-			}	
-		}else{
-			printf("%s is a special file.\n",inputPath);
+	if((dirP=opendir(inputPath))==NULL){
+		fprintf(stderr,"Could not open directory <%s>\n",strerror(errno));
+		return -errno;
+	}
+
+	//recursive while block, goes until entry is NULL or it finds a directory, then recurses to either return a value or read another directory
+	while((entry = readdir(dirP)) != NULL){
+
+		//skip over . and .. directories to avoid infinite recursion
+		if(strcmp(entry->d_name, ".")==0 || strcmp(entry->d_name, "..")==0){
+			continue;
 		}
-	}else{
-		printf("%s does not exist or is not reachable.\n",inputPath);
-	}	
-	return 0;
+		
+		char dirPath[PATH_MAX];
+		snprintf(dirPath,sizeof(dirPath),"%s/%s", inputPath, entry->d_name);
+	
+		//if block to check what the current file is
+		if(stat(dirPath,s)==0){
+			//if it is a file
+			if(S_ISREG (s->st_mode)){
+				printf("%s is a regular file.\n",entry->d_name);
+				if(access(dirPath,R_OK)==0){
+					printf("Can read <%s>\n",entry->d_name);
+					fileCount++;
+				}else{
+					printf("Cannot read <%s>\n",entry->d_name);
+				}
+			//if it is a directory
+			}else if(S_ISDIR(s->st_mode)){
+				printf("%s is a directory.\n",entry->d_name);
+
+				fileCount += readable(dirPath);
+
+				if(access(dirPath,F_OK)==0){
+					printf("Can access dir: <%s>\n",entry->d_name);
+					
+				}else{
+					printf("Unable to access dir: <%s>\n",entry->d_name);
+				}	
+			//if it is anything else (special etc.)
+			}else{
+				printf("%s is a special file.\n",entry->d_name);
+			}
+		}else{
+			printf("%s does not exist or is not reachable.\n",entry->d_name);
+		}	
+		
+	}
+	closedir(dirP);
+	printf("Readable files found: <%d>\n",fileCount);
+	return fileCount;
 }
 
 int main(int argc, char *argv[]){
